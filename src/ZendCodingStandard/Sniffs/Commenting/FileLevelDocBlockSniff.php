7df812cbdcc805f05d95d3cd59239ebae12c8c56
Matthew Weier O'Phinney <matthew@zend.com>
2017-08-01 12:28:13 -0500

    Refactors loop into its own function
    Refactors the loop over the file-level comment tags into the function
    `parseCommentTag()`, and refactors most conditional content within it
    into discrete named methods, allowing `parseCommentTag()` to act as a
    facade.

--- src/ZendCodingStandard/Sniffs/Commenting/FileLevelDocBlockSniff.php
+++ src/ZendCodingStandard/Sniffs/Commenting/FileLevelDocBlockSniff.php
@@ -7,6 +7,7 @@
 
 namespace ZendCodingStandard\Sniffs\Commenting;
 
+use ArrayObject;
 use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Sniffs\Sniff;
 use ZendCodingStandard\Utils\LicenseUtils;
@@ -142,81 +143,11 @@ class FileLevelDocBlockSniff implements Sniff
             '@license'   => true,
         ];
 
-        $foundTags = [];
+        $foundTags = new ArrayObject([]);
         foreach ($tokens[$commentStart]['comment_tags'] as $tag) {
-            $name       = $tokens[$tag]['content'];
-            $isRequired = isset($required[$name]);
-
-            if ($isRequired === true && in_array($name, $foundTags) === true) {
-                $error = 'Only one %s tag is allowed in a file-level DocBlock';
-                $data  = [$name];
-                $phpcsFile->addError($error, $tag, 'Duplicate' . ucfirst(substr($name, 1)) . 'Tag', $data);
-            }
-
-            $foundTags[] = $name;
-
-            if ($name === '@link') {
-                $error = 'Deprecated @link tag is used, use @see tag instead';
-                $fix   = $phpcsFile->addFixableError($error, $tag, 'DeprecatedLinkTag');
-                if ($fix === true) {
-                    $phpcsFile->fixer->replaceToken($tag, '@see ');
-                }
-            }
-
-            if ($isRequired === false && $name !== '@link') {
-                continue;
-            }
-
-            $string = $phpcsFile->findNext(T_DOC_COMMENT_STRING, $tag, $commentEnd);
-            if ($string === false || $tokens[$string]['line'] !== $tokens[$tag]['line']) {
-                $error = 'Content missing for %s tag in file-level DocBlock';
-                $data  = [$name];
-                $phpcsFile->addError($error, $tag, 'Empty' . ucfirst(substr($name, 1)) . 'Tag', $data);
-                continue;
-            }
-
-            if ($name === '@see' || $name === '@link') {
-                $expected = sprintf('https://github.com/%s for the canonical source repository', $this->repo);
-                if (preg_match('|^' . $expected . '$|', $tokens[$string]['content']) === 0) {
-                    $error = 'Expected "%s" for %s tag';
-                    $fix   = $phpcsFile->addFixableError($error, $tag, 'IncorrectSourceLink', [$expected, $name]);
-                    if ($fix === true) {
-                        $phpcsFile->fixer->replaceToken($string, $expected);
-                    }
-                }
-                continue;
-            }
-
-            if ($name === '@copyright') {
-                // Grab copyright date range
-                list($firstYear, $lastYear) = LicenseUtils::detectDateRange($tokens[$string]['content']);
-
-                $expected = sprintf('https://github.com/%s/blob/master/COPYRIGHT.md Copyright', $this->repo);
-                if (preg_match('|^' . $expected . '$|', $tokens[$string]['content']) === 0) {
-                    $error = 'Expected "%s" for @copyright tag';
-                    $fix   = $phpcsFile->addFixableError($error, $tag, 'IncorrectCopyrightLink', [$expected]);
-                    if ($fix === true) {
-                        $phpcsFile->fixer->replaceToken($string, $expected);
-                        if ($firstYear !== null) {
-                            LicenseUtils::buildFiles($firstYear, $lastYear);
-                        }
-                    }
-                }
-                continue;
-            }
-
-            if ($name === '@license') {
-                $expected = sprintf('https://github.com/%s/blob/master/LICENSE.md New BSD License', $this->repo);
-                if (preg_match('|^' . $expected . '$|', $tokens[$string]['content']) === 0) {
-                    $error = 'Expected "%s" for @license tag';
-                    $fix   = $phpcsFile->addFixableError($error, $tag, 'IncorrectLicenseLink', [$expected]);
-                    if ($fix === true) {
-                        $phpcsFile->fixer->replaceToken($string, $expected);
-                    }
-                }
-                continue;
-            }
+            $this->parseCommentTag($tag, $foundTags, $tokens, $required, $commentEnd, $phpcsFile);
         }
+        $foundTags = $foundTags->getArrayCopy();
 
         // If a @link tag was detected, it already triggered errors at this
         // point. Treat @link as @see to suppress even more errors and warnings
@@ -257,5 +188,167 @@ class FileLevelDocBlockSniff implements Sniff
 
         // Ignore the rest of the file.
         return ($phpcsFile->numTokens + 1);
+    }
+
+    /**
+     * @param string $tag
+     * @param ArrayObject $foundTags
+     * @param array $tokens
+     * @param array $required
+     * @param string $commentEnd
+     * @param File $phpcsFile
+     * @return void
+     */
+    private function parseCommentTag(
+        $tag,
+        ArrayObject $foundTags,
+        array $tokens,
+        array $required,
+        $commentEnd,
+        File $phpcsFile
+    ) {
+        $name       = $tokens[$tag]['content'];
+        $isRequired = isset($required[$name]);
+
+        $this->sniffForDuplicateRequiredTag($name, $tag, $isRequired, $foundTags, $phpcsFile);
+        $foundTags->append($name);
+        $this->sniffForLinkTag($name, $tag, $phpcsFile);
+
+        if ($isRequired === false && $name !== '@link') {
+            return;
+        }
+
+        $string = $phpcsFile->findNext(T_DOC_COMMENT_STRING, $tag, $commentEnd);
+        if ($string === false || $tokens[$string]['line'] !== $tokens[$tag]['line']) {
+            return $this->reportMissingTagContent($name, $tag, $phpcsFile);
+        }
+        if ($name === '@see' || $name === '@link') {
+            return $this->sniffForIncorrectSourceLink($name, $tag, $string, $tokens, $phpcsFile);
+        }
+        if ($name === '@copyright') {
+            return $this->sniffForIncorrectCopyrightLink($tag, $string, $tokens, $phpcsFile);
+        }
+        if ($name === '@license') {
+            return $this->sniffForIncorrectLicenseLink($tag, $string, $tokens, $phpcsFile);
+        }
+    }
+
+    /**
+     * @param string $name
+     * @param string $tag
+     * @param bool $isRequired
+     * @param ArrayObject $foundTags
+     * @param File $phpcsFile
+     * @return void
+     */
+    private function sniffForDuplicateRequiredTag($name, $tag, $isRequired, ArrayObject $foundTags, File $phpcsFile)
+    {
+        if ($isRequired !== true || in_array($name, $foundTags->getArrayCopy()) !== true) {
+            return;
+        }
+
+        $error = 'Only one %s tag is allowed in a file-level DocBlock';
+        $data  = [$name];
+        $phpcsFile->addError($error, $tag, 'Duplicate' . ucfirst(substr($name, 1)) . 'Tag', $data);
+    }
+
+    /**
+     * @param string $name
+     * @param string $tag
+     * @param File $phpcsFile
+     * @return void
+     */
+    private function sniffForLinkTag($name, $tag, File $phpcsFile)
+    {
+        if ($name !== '@link') {
+            return;
+        }
+
+        $error = 'Deprecated @link tag is used, use @see tag instead';
+        if (true === $phpcsFile->addFixableError($error, $tag, 'DeprecatedLinkTag')) {
+            $phpcsFile->fixer->replaceToken($tag, '@see ');
+        }
+    }
+
+    /**
+     * @param string $name
+     * @param string $tag
+     * @param File $phpcsFile
+     * @return void
+     */
+    private function reportMissingTagContent($name, $tag, File $phpcsFile)
+    {
+        $error = 'Content missing for %s tag in file-level DocBlock';
+        $data  = [$name];
+        $phpcsFile->addError($error, $tag, 'Empty' . ucfirst(substr($name, 1)) . 'Tag', $data);
+    }
+
+    /**
+     * @param string $name
+     * @param string $tag
+     * @param string $content
+     * @param array $tokens
+     * @param File $phpcsFile
+     * @return void
+     */
+    private function sniffForIncorrectSourceLink($name, $tag, $content, array $tokens, File $phpcsFile)
+    {
+        $expected = sprintf('https://github.com/%s for the canonical source repository', $this->repo);
+        if (0 !== preg_match('|^' . $expected . '$|', $tokens[$content]['content'])) {
+            return;
+        }
+
+        $error = 'Expected "%s" for %s tag';
+        if (true === $phpcsFile->addFixableError($error, $tag, 'IncorrectSourceLink', [$expected, $name])) {
+            $phpcsFile->fixer->replaceToken($content, $expected);
+        }
+    }
+
+    /**
+     * @param string $tag
+     * @param string $content
+     * @param array $tokens
+     * @param File $phpcsFile
+     * @return void
+     */
+    private function sniffForIncorrectCopyrightLink($tag, $content, array $tokens, File $phpcsFile)
+    {
+        // Grab copyright date range
+        list($firstYear, $lastYear) = LicenseUtils::detectDateRange($tokens[$content]['content']);
+
+        $expected = sprintf('https://github.com/%s/blob/master/COPYRIGHT.md Copyright', $this->repo);
+        if (0 !== preg_match('|^' . $expected . '$|', $tokens[$content]['content'])) {
+            return;
+        }
+
+        $error = 'Expected "%s" for @copyright tag';
+        if (true !== $phpcsFile->addFixableError($error, $tag, 'IncorrectCopyrightLink', [$expected])) {
+            return;
+        }
+
+        $phpcsFile->fixer->replaceToken($content, $expected);
+        if ($firstYear !== null) {
+            LicenseUtils::buildFiles($firstYear, $lastYear);
+        }
+    }
+
+    /**
+     * @param string $tag
+     * @param string $content
+     * @param array $tokens
+     * @param File $phpcsFile
+     * @return void
+     */
+    private function sniffForIncorrectLicenseLink($tag, $content, array $tokens, File $phpcsFile)
+    {
+        $expected = sprintf('https://github.com/%s/blob/master/LICENSE.md New BSD License', $this->repo);
+        if (0 !== preg_match('|^' . $expected . '$|', $tokens[$content]['content'])) {
+            return;
+        }
+
+        $error = 'Expected "%s" for @license tag';
+        if (true === $phpcsFile->addFixableError($error, $tag, 'IncorrectLicenseLink', [$expected])) {
+            $phpcsFile->fixer->replaceToken($content, $expected);
+        }
     }
 }
